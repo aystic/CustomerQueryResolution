79d4572be7aa2cc9c50b11b01ac13015ef796782
Mohit Pathak <itspmohit@gmail.com>
2022-12-29 23:26:38 +0530

    feat(app.js,app.css): fix the start screen and added the login api and email validation login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,47 +2,74 @@ import "./App.css";
 import { useState } from "react";
 import socket from "./utils/socket";
 import Chat from "./components/chat";
+import { login } from "./api/login";
+
+const EMAIL_REGEX =
+	/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 function App() {
-	const [user, setUser] = useState("");
-	const [isUser, setIsUser] = useState(false);
-	const [isLoggedIn, setIsLoggedIn] = useState(true);
+	const [email, setEmail] = useState("");
+	const [userData, setUserData] = useState({
+		email: "itspmohit@gmail.com",
+		resolved: [],
+		current: [],
+	});
+	const [isUser, setIsUser] = useState(true);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [isValid, setValidity] = useState(false);
 	const inputChangeHandler = (e) => {
-		setUser(e.target.value);
+		setEmail(e.target.value);
+		setValidity(EMAIL_REGEX.test(e.target.value));
 	};
-	const startChatHandler = (e) => {
+	const startChatHandler = async (e) => {
 		e.preventDefault();
-		// socket.connect();
-
-		// In production we will fetch the role of the user from the backend
-		if (user.split("@")[1] === "agent.com") {
-			setIsUser(false);
-		} else {
-			setIsUser(true);
+		try {
+			// const data = await login(email);
+			const data = {
+				email,
+				resolved: [],
+				current: [],
+			};
+			console.log(data);
+			setUserData(data);
+			// In production we will fetch the role of the user from the backend
+			if (email.split("@")[1] === "agent.com") {
+				setIsUser(false);
+			} else {
+				setIsUser(true);
+			}
+			setIsLoggedIn(true);
+		} catch (err) {
+			console.error(err);
 		}
-		setIsLoggedIn(true);
 	};
 	return (
 		<>
 			{isLoggedIn && (
 				<div className="chat-container">
-					<Chat isUser={isUser} />
+					<Chat userData={userData} isUser={isUser} />
 				</div>
 			)}
 			{!isLoggedIn && (
-				<div className="start-screen">
-					<form onSubmit={startChatHandler}>
-						<input
-							className="username-input"
-							placeholder="Enter your username"
-							type={"email"}
-							value={user}
-							onChange={inputChangeHandler}
-						/>
-						<button className="username-input-btn" type="submit">
-							Enter
-						</button>
-					</form>
+				<div className="start-page">
+					<div className="start-form-container">
+						<form onSubmit={startChatHandler}>
+							<input
+								className="username-input"
+								placeholder="Enter your email"
+								type={"email"}
+								value={email}
+								onChange={inputChangeHandler}
+							/>
+							<button
+								disabled={!isValid}
+								className="username-input-btn"
+								type="submit"
+							>
+								Enter
+							</button>
+						</form>
+					</div>
 				</div>
 			)}
 		</>
