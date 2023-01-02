e3e7f74e360a0b783634bacab0e5f4f15ff812c8
Mohit Pathak <itspmohit@gmail.com>
2023-01-03 02:22:50 +0530

    fix: fix socket client url

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,7 +1,10 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:8000/";
-const socket = io(URL, { autoConnect: false });
+console.log(process.env.REACT_APP_WEBSOCKET_URL);
+const socket = io(process.env.REACT_APP_WEBSOCKET_URL, {
+	autoConnect: false,
+	path: "/ws/",
+});
 
 socket.onAny((event, ...args) => {
 	console.log(event, args);
