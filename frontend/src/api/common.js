f95ca8fe6dc10dc31795bb808617b2f760567e34
Mohit Pathak <itspmohit@gmail.com>
2023-01-03 02:26:18 +0530

    feat :add apis

--- frontend/src/api/common.js
+++ frontend/src/api/common.js
@@ -1,13 +1,65 @@
 import axios from "axios";
 export const login = async (email) => {
 	try {
-		const data = await axios.post(
-			`${process.env.REACT_APP_BACKEND_URL}/login`,
+		const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}`, {
+			email: `${email}`,
+		});
+		return response.data.data[0];
+	} catch (err) {
+		throw new Error(err);
+	}
+};
+
+export const getChat = async (id, email) => {
+	try {
+		const response = await axios.post(
+			`${process.env.REACT_APP_BACKEND_URL}/chat`,
 			{
+				id,
 				email,
 			}
 		);
-		return data;
+		return response.data.data;
+	} catch (err) {
+		throw new Error(err);
+	}
+};
+
+export const getChatMessages = async (chatID) => {
+	try {
+		const response = await axios.post(
+			`${process.env.REACT_APP_BACKEND_URL}/messages`,
+			{
+				id: chatID,
+			}
+		);
+		return response.data.data;
+	} catch (err) {
+		throw new Error(err);
+	}
+};
+
+export const markAsResolve = async (postData) => {
+	try {
+		const response = await axios.post(
+			`${process.env.REACT_APP_BACKEND_URL}/mark-as-resolved`,
+			postData
+		);
+		return response.data.data;
+	} catch (err) {
+		throw new Error(err);
+	}
+};
+
+export const getUserDetails = async (userID) => {
+	try {
+		const response = await axios.post(
+			`${process.env.REACT_APP_BACKEND_URL}/getUserDetails`,
+			{
+				userID,
+			}
+		);
+		return response.data.data;
 	} catch (err) {
 		throw new Error(err);
 	}
--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -2,9 +2,10 @@ import axios from "axios";
 export const createChat = async (postData) => {
 	try {
 		const data = await axios.post(
-			`${process.env.REACT_APP_BACKEND_URL}/newchat`
+			`${process.env.REACT_APP_BACKEND_URL}/new-chat`,
+			postData
 		);
-		console.log(data);
+		return data.data;
 	} catch (err) {
 		throw new Error(err);
 	}
