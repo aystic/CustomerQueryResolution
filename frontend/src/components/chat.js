3fac371077ea7065d9a9f762d276e704ed556053
Mohit Pathak <itspmohit@gmail.com>
2023-01-03 02:25:27 +0530

    feat :add chat selection and deselection functionality and integrated sidebar with chat window

--- frontend/src/components/chat.js
+++ frontend/src/components/chat.js
@@ -2,29 +2,72 @@ import classes from "./chat.module.css";
 import TopBar from "./topBar";
 import Sidebar from "./sidebar";
 import ChatWindow from "./chatwindow";
-import { useMemo, useState } from "react";
+import { useMemo, useState, useContext } from "react";
+import { GlobalContext } from "../store/globalContext";
+import { getUserDetails } from "../api/common";
 
-const Chat = ({ isUser, userData, showNotification }) => {
+const Chat = () => {
+	const globalContext = useContext(GlobalContext);
+	const [chat, setChat] = useState([]);
 	const [chatTag, setChatTag] = useState("current");
 	const [selectedChat, setSelectedChat] = useState(null);
+	const [receiverEmail, setReceiverEmail] = useState(null);
+
+	const receiverChangeHandler = async (userID) => {
+		try {
+			if (userID) {
+				const data = await getUserDetails(userID);
+				setReceiverEmail(data[0].emailID);
+			} else {
+				setReceiverEmail(null);
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	};
+	const chatSelectHandler = (chatID) => {
+		setSelectedChat(chatID);
+	};
+	const addMessageHandler = (message) => {
+		setChat((prev) => {
+			const newMessage = {
+				message,
+				sender: globalContext.isUser ? "user" : "agent",
+				timestamp: new Date().toISOString(),
+				_linked_to: null,
+				userID: globalContext.userData.id,
+				chatID: selectedChat,
+				type: "message",
+			};
+			return [...prev, newMessage];
+		});
+	};
 	const tagChangeHandler = useMemo(() => {
 		return (chatTag) => setChatTag(chatTag);
 	}, []);
 	return (
 		<>
 			<TopBar
-				userData={userData}
-				isUser={isUser}
 				classes={classes}
 				tagChangeHandler={tagChangeHandler}
 				chatTag={chatTag}
+				receiverEmail={receiverEmail}
 			/>
 			<div className={classes["chat-body"]}>
-				<Sidebar userData={userData} classes={classes} chatTag={chatTag} />
-				<ChatWindow
+				<Sidebar
+					setChat={setChat}
 					selectedChat={selectedChat}
+					chatSelectHandler={chatSelectHandler}
 					classes={classes}
-					showNotification={showNotification}
+					chatTag={chatTag}
+					receiverChangeHandler={receiverChangeHandler}
+				/>
+				<ChatWindow
+					chat={chat}
+					chatTag={chatTag}
+					selectedChat={selectedChat}
+					addMessageHandler={addMessageHandler}
+					classes={classes}
 				/>
 			</div>
 		</>
--- frontend/src/components/chatwindow.js
+++ frontend/src/components/chatwindow.js
@@ -1,40 +1,38 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useContext } from "react";
+import { GlobalContext } from "../store/globalContext";
 import NewChat from "./newChat";
 import sendBtn from "./send-button.svg";
-const messages = [
-	{ text: "hello", self: true },
-	{ text: "world", self: false },
-	{
-		text: "Cras suscipit ultricies nisl at tristique. Integer porttitor erat metus, non tempus metus porttitor a. Aliquam rutrum arcu in tincidunt vulputate. Pellentesque purus turpis, ultricies vel nunc id, gravida ornare nisl. Duis eu dolor augue. Curabitur condimentum neque eu nisi suscipit consectetur. Aliquam hendrerit, dui id tempus pulvinar, dolor nulla malesuada neque, sed facilisis ante diam porttitor felis. Suspendisse nunc justo, consectetur vel ultrices eu, fringilla in erat. Fusce a cursus augue. Vivamus in cursus quam. Aenean sed ex quis turpis laoreet elementum ut id mauris. Curabitur vel hendrerit lorem, sed semper leo. Phasellus neque mauris, interdum vehicula iaculis non, scelerisque ac nisl. Sed imperdiet est risus, nec commodo massa malesuada id. Quisque id lorem at magna volutpat imperdiet.",
-		self: false,
-	},
-];
-const ChatWindow = ({ classes, selectedChat, showNotification }) => {
-	const [chats, setChats] = useState(messages);
+const ChatWindow = ({
+	chat,
+	classes,
+	selectedChat,
+	addMessageHandler,
+	chatTag,
+}) => {
+	const globalContext = useContext(GlobalContext);
 	const [message, setMessage] = useState("");
 	const scrollToRef = useRef(null);
 	const chatHandler = (e) => {
 		e.preventDefault();
-		setChats((prev) => {
-			return [...prev, { text: message, self: true }];
-		});
+		addMessageHandler(message);
 		setMessage("");
 	};
 	const messageChangeHandler = (e) => {
 		setMessage(e.target.value);
 	};
 
-	const chatList = chats.map((value, idx) => {
+	const chatList = chat.map((value, idx) => {
 		return (
 			<div
 				key={idx}
 				className={
-					value.self === true
+					(value.sender === "user" && globalContext.isUser === true) ||
+					(value.sender === "agent" && globalContext.isUser === false)
 						? classes["message-self"]
 						: classes["message-other"]
 				}
 			>
-				{value.text}
+				{value.message}
 			</div>
 		);
 	});
@@ -44,10 +42,10 @@ const ChatWindow = ({ classes, selectedChat, showNotification }) => {
 				behavior: "smooth",
 			});
 		}
-	}, [chats]);
+	}, [chat]);
 	chatList.push(
 		<div
-			key={chats.length}
+			key={chat.length}
 			ref={scrollToRef}
 			className={classes["scroll-to-div"]}
 		></div>
@@ -62,28 +60,35 @@ const ChatWindow = ({ classes, selectedChat, showNotification }) => {
 					>
 						{chatList}
 					</div>
-					<div className={classes["chat-input-container"]}>
-						<form className={classes["form-container"]} onSubmit={chatHandler}>
-							<input
-								value={message}
-								onChange={messageChangeHandler}
-								className={classes["chat-input"]}
-								type={"text"}
-								placeholder="Type your message here!"
-							></input>
-							<button
-								disabled={message === ""}
-								style={{ visibility: message === "" ? "hidden" : "visible" }}
-								className={classes["chat-input-btn"]}
-								type="submit"
+					{chatTag === "current" && (
+						<div className={classes["chat-input-container"]}>
+							<form
+								className={classes["form-container"]}
+								onSubmit={chatHandler}
 							>
-								<img src={sendBtn} alt="send button" />
-							</button>
-						</form>
-					</div>
+								<input
+									value={message}
+									onChange={messageChangeHandler}
+									className={classes["chat-input"]}
+									type={"text"}
+									placeholder="Type your message here!"
+								></input>
+								<button
+									disabled={message === ""}
+									style={{ visibility: message === "" ? "hidden" : "visible" }}
+									className={classes["chat-input-btn"]}
+									type="submit"
+								>
+									<img src={sendBtn} alt="send button" />
+								</button>
+							</form>
+						</div>
+					)}
 				</>
+			) : globalContext.isUser ? (
+				<NewChat />
 			) : (
-				<NewChat showNotification={showNotification} />
+				""
 			)}
 		</div>
 	);
