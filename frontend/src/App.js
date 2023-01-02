2f2c4ac242df91cec9d247c5d6ff1a9da65c091a
Mohit Pathak <itspmohit@gmail.com>
2023-01-03 02:29:06 +0530

    feat: add global context and fix socket connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,55 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useContext } from "react";
 import socket from "./utils/socket";
+import { GlobalContext } from "./store/globalContext";
 import { login } from "./api/common";
+import { getChat } from "./api/common";
 import Chat from "./components/chat";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 const EMAIL_REGEX =
 	/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 function App() {
 	const [email, setEmail] = useState("");
-	const [userData, setUserData] = useState({
-		email: "itspmohit@gmail.com",
-		resolved: [],
-		current: [],
-	});
-	const [isUser, setIsUser] = useState(true);
-	const [isLoggedIn, setIsLoggedIn] = useState(true);
+	const globalContext = useContext(GlobalContext);
 	const [isValid, setValidity] = useState(false);
 	const inputChangeHandler = (e) => {
 		setEmail(e.target.value);
 		setValidity(EMAIL_REGEX.test(e.target.value));
 	};
+
 	const startChatHandler = async (e) => {
 		e.preventDefault();
 		try {
-			// const data = await login(email);
-			const data = {
-				email,
+			const data = await login(email);
+			const userData = await getChat(data.id, data.emailID);
+			socket.auth = { email: data.emailID };
+			socket.connect();
+
+			const userChatData = {
+				id: data.id,
+				email: data.emailID,
 				resolved: [],
 				current: [],
 			};
-			console.log(data);
-			setUserData(data);
-			// In production we will fetch the role of the user from the backend
-			if (email.split("@")[1] === "agent.com") {
-				setIsUser(false);
+			userData.forEach((val) => {
+				if (val.resolved === 1) {
+					userChatData.resolved.push(val);
+				} else {
+					userChatData.current.push(val);
+				}
+			});
+			globalContext.setIsLoggedIn(true);
+			globalContext.setUserData(userChatData);
+			if (data.emailID.split("@")[1] === "agent.com") {
+				globalContext.setIsUser(false);
 			} else {
-				setIsUser(true);
+				globalContext.setIsUser(true);
 			}
-			setIsLoggedIn(true);
 		} catch (err) {
 			console.error(err);
 		}
-	};
-
-	const showNotification = ({ type, value }) => {
-		toast(value, { type, className: "notification" });
 	};
 
 	return (
@@ -64,16 +66,12 @@ function App() {
 				pauseOnHover
 				theme="dark"
 			/>
-			{isLoggedIn && (
+			{globalContext.isLoggedIn && (
 				<div className="chat-container">
-					<Chat
-						userData={userData}
-						isUser={isUser}
-						showNotification={showNotification}
-					/>
+					<Chat />
 				</div>
 			)}
-			{!isLoggedIn && (
+			{!globalContext.isLoggedIn && (
 				<div className="start-page">
 					<div className="start-form-container">
 						<form onSubmit={startChatHandler}>
